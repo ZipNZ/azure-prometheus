ae14708f627a678a745a16bad3c3fda39f39bc91
robck <brockm88@gmail.com>
2019-09-30 22:37:36 +1300

    Add configurable scrape endpoint

--- new-entry-point.sh
+++ new-entry-point.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
+
+if [ ! -z "$SCRAPE_ENDPOINT" ]
+then
+    sed -i s/localhost:9090/$SCRAPE_ENDPOINT/g /etc/prometheus/prometheus.yml
+fi
+
 exec "$@" --web.listen-address="0.0.0.0:${PORT}"
